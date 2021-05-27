3b31e21ffee7d14ee6d865f60347911bf975a0a3
Tomas Amoretti <tom.ia@hotmail.com>
2021-05-27 14:24:29 -0300

    sidecart y otros

--- checkout/checkout-ui-custom/checkout6-custom.js
+++ checkout/checkout-ui-custom/checkout6-custom.js
@@ -16,6 +16,12 @@ $(document).ready(function(){
       }
     }
 
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = '/arquivos/favicon.ico';
+    document.getElementsByTagName('head')[0].appendChild(link);
+
 });
 
 
