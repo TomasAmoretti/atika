66ae19dcbaabe290c40b05c252bf01995884be36
Tomas Amoretti <tom.ia@hotmail.com>
2021-08-02 17:48:00 -0300

    cambios finales lunes 2

--- descripcion-larga/react/FichaTecnica.tsx
+++ descripcion-larga/react/FichaTecnica.tsx
@@ -1,72 +1,72 @@
-import React, {  } from 'react'
-import { useCssHandles } from 'vtex.css-handles'
-import { useQuery } from 'react-apollo'
-import useProduct from 'vtex.product-context/useProduct'
-import productFicha from './queries/productFicha.graphql'
-import { FichatecnicaString } from './utils/ficha-tecnica';
+import React from "react";
+import { useCssHandles } from "vtex.css-handles";
+import { useQuery } from "react-apollo";
+import useProduct from "vtex.product-context/useProduct";
+import productFicha from "./queries/productFicha.graphql";
+import { FichatecnicaString } from "./utils/ficha-tecnica";
 
-interface FichaTecnica {
-}
+interface FichaTecnica {}
 
 const FichaTecnica: StorefrontFunctionComponent<FichaTecnica> = ({}) => {
-
-  const CSS_HANDLES = ['fichaTecnica']
-  const handles = useCssHandles(CSS_HANDLES)
-  const { product } = useProduct()
+  const CSS_HANDLES = ["fichaTecnica"];
+  const handles = useCssHandles(CSS_HANDLES);
+  const { product } = useProduct();
   const { data, loading, error } = useQuery(productFicha, {
     variables: {
       slug: product?.linkText,
     },
-    ssr: false
-  })
-
+    ssr: false,
+  });
 
   if (!product) {
     return (
       <div>
         <span>No es pagina de producto.</span>
       </div>
-    )
+    );
   }
   if (loading) {
     return (
       <div>
         <span></span>
       </div>
-    )
+    );
   }
   if (error) {
     return (
       <div>
         <span></span>
       </div>
-    )
+    );
   }
- 
+
   var fichaTecnica = data.product.items[0].referenceId[0].Value;
-  
-  if (FichatecnicaString.indexOf(fichaTecnica) != -1){
+
+  if (FichatecnicaString.indexOf(fichaTecnica) != -1) {
     return (
-      <a className={`${handles.fichaTecnica} db tc`} href={`https://www.sitios.knownonline.com/atika/${fichaTecnica}-FT.pdf`} target="_blank">
-        <img title="ficha-tecnica" src="/arquivos/ficha-tecnica2.png" alt="Ficha Tecnica" className={`${handles.fichaTecnica}`}/>
+      <a
+        className={`${handles.fichaTecnica} db tc`}
+        href={`https://www.sitios.knownonline.com/atika/${fichaTecnica}-FT.pdf`}
+        target="_blank"
+      >
+        <img
+          title="ficha-tecnica"
+          src="/arquivos/ficha-tecnica2.svg"
+          alt="Ficha Tecnica"
+          className={`${handles.fichaTecnica}`}
+        />
       </a>
-    )
+    );
   }
 
-  return (
-    <div className={`${handles.fichaTecnica} db tc`}>
-
-    </div>
-  )
-
-}
+  return <div className={`${handles.fichaTecnica} db tc`}></div>;
+};
 
 FichaTecnica.schema = {
-  title: 'ficha-tecnica.title',
-  description: 'ficha-tecnica.description',
-  type: 'object',
-  properties: {
-  },
-}
+  title: "ficha-tecnica.title",
+  description: "ficha-tecnica.description",
+  type: "object",
+  properties: {},
+};
 
-export default FichaTecnica
+export default FichaTecnica;
--- descripcion-larga/react/Instalacion.tsx
+++ descripcion-larga/react/Instalacion.tsx
@@ -1,86 +1,104 @@
-import React, {  } from 'react'
-import { useCssHandles } from 'vtex.css-handles'
-import { useQuery } from 'react-apollo'
-import useProduct from 'vtex.product-context/useProduct'
-import productFicha from './queries/productFicha.graphql'
-import { InstalacionPorcelanato } from './utils/instalacion';
-import { InstalacionBionly } from './utils/instalacion';
-import { InstalacionFloobest } from './utils/instalacion';
+import React from "react";
+import { useCssHandles } from "vtex.css-handles";
+import { useQuery } from "react-apollo";
+import useProduct from "vtex.product-context/useProduct";
+import productFicha from "./queries/productFicha.graphql";
+import { InstalacionPorcelanato } from "./utils/instalacion";
+import { InstalacionBionly } from "./utils/instalacion";
+import { InstalacionFloobest } from "./utils/instalacion";
 
-interface Instalacion {
-}
+interface Instalacion {}
 
 const Instalacion: StorefrontFunctionComponent<Instalacion> = ({}) => {
-
-  const CSS_HANDLES = ['Instalacion']
-  const handles = useCssHandles(CSS_HANDLES)
-  const { product } = useProduct()
+  const CSS_HANDLES = ["Instalacion"];
+  const handles = useCssHandles(CSS_HANDLES);
+  const { product } = useProduct();
   const { data, loading, error } = useQuery(productFicha, {
     variables: {
       slug: product?.linkText,
     },
-    ssr: false
-  })
-
+    ssr: false,
+  });
 
   if (!product) {
     return (
       <div>
         <span>No es pagina de producto.</span>
       </div>
-    )
+    );
   }
   if (loading) {
     return (
       <div>
         <span></span>
       </div>
-    )
+    );
   }
   if (error) {
     return (
       <div>
         <span></span>
       </div>
-    )
+    );
   }
 
   var Instalacion = data.product.items[0].referenceId[0].Value;
-  
-  if (InstalacionPorcelanato.indexOf(Instalacion) != -1){
+
+  if (InstalacionPorcelanato.indexOf(Instalacion) != -1) {
     return (
-      <a className={`${handles.Instalacion} db tc`} href={`https://www.sitios.knownonline.com/atika/RECOMENDACIONES-DE-INSTALACIÓN-ATIKA-PORCELANATOS-Y-CERAMICOS.pdf`} target="_blank">
-        <img title="ficha-tecnica" src="/arquivos/instalacion.PNG" alt="Instalacion" className={`${handles.Instalacion}`}/>
+      <a
+        className={`${handles.Instalacion} db tc`}
+        href={`https://www.sitios.knownonline.com/atika/RECOMENDACIONES-DE-INSTALACIÓN-ATIKA-PORCELANATOS-Y-CERAMICOS.pdf`}
+        target="_blank"
+      >
+        <img
+          title="ficha-tecnica"
+          src="/arquivos/instalacion.svg"
+          alt="Instalacion"
+          className={`${handles.Instalacion}`}
+        />
       </a>
-    )
-  }else if (InstalacionBionly.indexOf(Instalacion) != -1){
+    );
+  } else if (InstalacionBionly.indexOf(Instalacion) != -1) {
     return (
-      <a className={`${handles.Instalacion} db tc`} href={`https://www.sitios.knownonline.com/atika/RECOMENDACIONES-DE-INSTALACIÓN-ATIKA-ORGÁNICOS-BIONYL.pdf`} target="_blank">
-        <img title="ficha-tecnica" src="/arquivos/instalacion.PNG" alt="Instalacion" className={`${handles.Instalacion}`}/>
+      <a
+        className={`${handles.Instalacion} db tc`}
+        href={`https://www.sitios.knownonline.com/atika/RECOMENDACIONES-DE-INSTALACIÓN-ATIKA-ORGÁNICOS-BIONYL.pdf`}
+        target="_blank"
+      >
+        <img
+          title="ficha-tecnica"
+          src="/arquivos/instalacion.svg"
+          alt="Instalacion"
+          className={`${handles.Instalacion}`}
+        />
       </a>
-    )
-  }else if (InstalacionFloobest.indexOf(Instalacion) != -1){
+    );
+  } else if (InstalacionFloobest.indexOf(Instalacion) != -1) {
     return (
-      <a className={`${handles.Instalacion} db tc`} href={`https://www.sitios.knownonline.com/atika/RECOMENDACIONES-DE-INSTALACIÓN-ATIKA-VINILICOS-FLOORBEST.pdf`} target="_blank">
-        <img title="ficha-tecnica" src="/arquivos/instalacion.PNG" alt="Instalacion" className={`${handles.Instalacion}`}/>
+      <a
+        className={`${handles.Instalacion} db tc`}
+        href={`https://www.sitios.knownonline.com/atika/RECOMENDACIONES-DE-INSTALACIÓN-ATIKA-VINILICOS-FLOORBEST.pdf`}
+        target="_blank"
+      >
+        <img
+          title="ficha-tecnica"
+          src="/arquivos/instalacion.svg"
+          alt="Instalacion"
+          className={`${handles.Instalacion}`}
+        />
       </a>
-    )
+    );
   }
 
-  return (
-    <div className={`${handles.Instalacion} db tc`}>
-
-    </div>
-  )
-
-}
+  return <div className={`${handles.Instalacion} db tc`}></div>;
+};
 
 Instalacion.schema = {
-  title: 'Instalacion.title',
-  description: 'Instalacion.description',
-  type: 'object',
-  properties: {
-  },
-}
+  title: "Instalacion.title",
+  description: "Instalacion.description",
+  type: "object",
+  properties: {},
+};
 
-export default Instalacion
+export default Instalacion;
