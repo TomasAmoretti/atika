5ab5bcfc8c9ac96436e94ec0f8ecdd341985a585
Tomas Amoretti <tom.ia@hotmail.com>
2021-08-26 19:21:25 -0300

    cambio unitario por cantidad en seller producto

--- descripcion-larga/react/InfoPrecioCuadrado.tsx
+++ descripcion-larga/react/InfoPrecioCuadrado.tsx
@@ -1,6 +1,7 @@
 import React, {  } from 'react'
 import { useCssHandles } from 'vtex.css-handles'
 import { useProduct } from 'vtex.product-context'
+import { getDefaultSeller } from './modules/seller'
 
 interface InfoPrecioCuadradoProps {
 }
@@ -12,28 +13,31 @@ const InfoPrecioCuadrado: StorefrontFunctionComponent<InfoPrecioCuadradoProps> =
   const { product } = useProduct()
 
   const productContextValue = useProduct()
-  const unitMultiplier = productContextValue?.selectedItem?.unitMultiplier ?? 1
 
-  if (!product) {
+  const seller = getDefaultSeller(productContextValue?.selectedItem?.sellers)
+
+  const commercialOffer = seller?.commertialOffer
+
+  if (!commercialOffer || commercialOffer?.AvailableQuantity <= 0) {
     return null
   }
 
-  if (unitMultiplier*1000 !== 1){
-    return (
-        <div className={`${handles.InfoPrecioCuadrado} db tc`}>
-        <div className="vtex-rich-text-0-x-container vtex-rich-text-0-x-container--titulo-m2">
-            <div className="vtex-rich-text-0-x-wrapper vtex-rich-text-0-x-wrapper--titulo-m2">
-                <p className="vtex-rich-text-0-x-paragraph vtex-rich-text-0-x-paragraph--titulo-m2">
-                    Ingresa los m2 que necesitas cubrir. Venta por caja<br/> Se recomienda agregar un 10% adicional a los m2 a cubrir
-                </p>
-            </div>
-        </div>
-        </div>
-    )
+  if (!product) {
+    return null;
   }
 
-  return null
-
+  return (
+    <div className={`${handles.InfoPrecioCuadrado} db tc`}>
+      <div className="vtex-rich-text-0-x-container vtex-rich-text-0-x-container--titulo-m2">
+        <div className="vtex-rich-text-0-x-wrapper vtex-rich-text-0-x-wrapper--titulo-m2">
+          <p className="vtex-rich-text-0-x-paragraph vtex-rich-text-0-x-paragraph--titulo-m2">
+            Ingresa los m2 que necesitas cubrir. Venta por caja
+            <br /> Se recomienda agregar un 10% adicional a los m2 a cubrir
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 InfoPrecioCuadrado.schema = {
